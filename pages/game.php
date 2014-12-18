38aa4358f558d643f48e182c8057d32e303f2a8e
David Ayers <john.d.ayers@gmail.com>
2014-12-18 13:40:50 -0500

    Fixed slight bug

--- pages/game.php
+++ pages/game.php
@@ -1,21 +1,5 @@
-<?php require_once('inc/header.inc'); ?>
-<?php require_once('php/errors_functions.php'); ?>
+<?php require_once('php/message_functions.php'); ?>
 
-<body>
-
-<div id="parchment"></div>
-<div id="border"></div>
-<div id="wrapper">
-	<div id="wrapper-inner">
-		<p class="version"><?php require_once('inc/version.inc'); ?></p>
-        <div id="map">
-        	<?php require_once('php/map_functions.php'); ?>
-        </div>
-
-	</div>
+<div id="map">
+	<?php require_once('php/map_functions.php'); ?>
 </div>
-
-<?php require_once('inc/footer.inc'); ?>
-
-</body>
-</html>
